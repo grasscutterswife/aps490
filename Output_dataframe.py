f71312a7d5e7689b09d489971c876ab93c36e3da
grasscutterswife <71188572+grasscutterswife@users.noreply.github.com>
2022-03-13 17:44:05 -0400

    Update Output_dataframe.py

--- Output_dataframe.py
+++ Output_dataframe.py
@@ -14,7 +14,6 @@ def load_data(nrows):
     data =pd.plotting(fig, nrows=nrows)
     data.remname(axis ='y',inplace = True)
     data['x']=pd.to_datatime(data["x"])
-    data[outlier_dates]=df.style.highlight_max(axis= 'y'))
     return data
 
 data =load_data[1000]
